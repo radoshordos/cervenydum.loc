76ab8975301029a023aa40be7b3eedf5068f31bb
Radek Horáček Bc <radek.horacek@office.cendis.cz>
2023-01-24 00:57:28 +0100

    update 2023-01-22 - basic auth

---
+++ app/Http/Middleware/BasicAuth.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        header('Cache-Control: no-cache, must-revalidate, max-age=0');
+        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+        $is_not_authenticated = (!$has_supplied_credentials || $_SERVER['PHP_AUTH_USER'] !== 'a' || $_SERVER['PHP_AUTH_PW'] !== 'a');
+        if ($is_not_authenticated) {
+            header('HTTP/1.1 401 Authorization Required');
+            header('WWW-Authenticate: Basic realm="Access denied"');
+            exit;
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\ProjectPartnersController;
 use App\Http\Controllers\SouvisejiciProjektyController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::resource('/', AktualityController::class)->names('aktuality')->only(['index']);
 Route::resource('/byty', BytyController::class)->names('byty')->only(['index']);
 Route::resource('/partneri', ProjectPartnersController::class)->names('partneri')->only(['index']);
 Route::resource('/souvisejici-projekty', SouvisejiciProjektyController::class)->names('souvisejici')->only(['index']);
 Route::resource('/pro-zajemce', CandidateController::class)->names('zajemci')->only(['index', 'store']);
+
